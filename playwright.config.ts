eef5000523c4b81392297419979ff52150c2cd58
Ajay Venkatesh Gunturu <ajayiiitongole@gmail.com>
2026-01-30 22:24:00 +0530

    Remove storybook build and wait

--- playwright.config.ts
+++ playwright.config.ts
@@ -47,7 +47,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run storybook',
+    command: process.env.CI ? 'npx http-server storybook-static -p 6006' : 'npm run storybook',
     port: 6006,
     reuseExistingServer: !process.env.CI,
   },
