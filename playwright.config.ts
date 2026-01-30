231ee36baf89b25b026a9522b085b82ef686a4f6
Ajay Venkatesh Gunturu <ajayiiitongole@gmail.com>
2026-01-30 21:42:12 +0530

    include tests for mobile as wellk

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,37 +20,29 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'Chrome Deskotp',
+      name: 'Deskotp Chrome',
       use: { ...devices['Desktop Chrome'] },
     },
     {
-      name: 'Chrome Deskotp',
-      use: { ...devices['Desktop Chrome'] },
-    },
-    {
-      name: 'webkit',
+      name: 'Desktop Safari',
       use: { ...devices['Desktop Safari'] },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    {
+      name: 'Mobile Portrait Chrome',
+      use: { ...devices['iPhone 12 Pro'] },
+    },
+    {
+      name: 'Mobile Portrait Safari',
+      use: { ...devices['iPhone 12 Pro'] },
+    },
+    {
+      name: 'Mobile Landscape Chrome',
+      use: { ...devices['iPhone 12 Pro landscape'] },
+    },
+    {
+      name: 'Mobile Landscape Safari',
+      use: { ...devices['iPhone 12 Pro landscape'] },
+    },
   ],
 
   /* Run your local dev server before starting the tests */
